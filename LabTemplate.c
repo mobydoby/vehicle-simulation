4dbb69d6659f704ad9cb178d88f220a2dfb699e5
thedomino1313 <dominic.c.beyer@gmail.com>
2022-02-04 10:24:26 -0500

    Most of Maddy's changes implemented

--- LabTemplate.c
+++ LabTemplate.c
@@ -10,16 +10,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Port_Init(void);               // Initialize all the GPIO ports (I/O only)
-void PCA_Init(void);                // Initialize PCA function
-void XBR_Init(void);                // Initialize Crossbar function
-void Interrupt_Init(void);          //Initialize interrupts function
-void SMB_Init(void);                // Initialize I2C function
-void read_ranger(void);             // Function to read the ranger and start a ping
-void read_compass(void);            // Function to read the compass heading
-void set_servoPW(signed int user_input);
-void set_motorPW(signed int user_input);
-void ReadRanger(uint8_t direction);
+void Port_Init(void);                // Initialize all the GPIO ports (I/O only)
+void PCA_Init(void);                 // Initialize PCA function
+void XBR_Init(void);                 // Initialize Crossbar function
+void Interrupt_Init(void);           //Initialize interrupts function
+void SMB_Init(void);                 // Initialize I2C function
+void read_ranger(uint8_t direction); // Reads the ranger specified by the direction parameter
+void update_rangers(void);           // Updates all distance variables to current values using read_ranger calls
+void read_compass(void);             // Function to read the compass heading
+void set_servoPW(signed int user_input); //Sets the servo PW according to desired direction
+void set_motorPW(signed int user_input); //Sets the motor PW to desired pulsewidth value
+void update_heading(void);           // Updates heading variable to current compass measurement
+void print_info(void);               // Prints a selection of important info
 
 signed int MOTOR_NEUT = 2765;       // Neutral              1.5 ms
 signed int MOTOR_MAX = 3502;        // Full Forward         1.9 ms
@@ -38,31 +40,34 @@ unsigned char new_print = 0;        // Flag denoting that we can print (set in i
 unsigned char r_count = 0;          // Counter to count for ranger delay
 unsigned char h_count = 0;          // Counter to count for compass delay
 unsigned char p_count = 0;          // Counter to count print delay
-signed int heading = 0;             // Variable to hold compass heading value
-unsigned int range = 0;             // Variable to hold ranger distance value
+
 unsigned int time = 0;
 uint8_t Data[2];
-signed int des_heading = 0;         // Desired heading
-signed int heading_error = 0;       // Error calculation
-uint16_t distance = 0;
-uint16_t distancef = 0;
-uint16_t distancel = 0;
-uint16_t distancer = 0;
-uint8_t front = 0xE0;
-uint8_t left = 0xE4;
-uint8_t right = 0xE6;
 
-uint8_t state = 1;
+signed int heading = 0;             // Variable to hold compass heading value
+signed int des_heading = 0;         // Variable to hold desired heading in deg/10
+signed int heading_error = 0;       // heading_error = des_heading - heading [set in switch statement]
 
-uint16_t desired_distance = 50;
-signed int distance_error = 0;
+uint16_t distance = 0;              // Temporary variable to hold most recent value of ANY ranger
+uint16_t distancef = 0;             // Variable to hold front ranger distance value
+uint16_t distancel = 0;             // Variable to hold left ranger distance value
+uint16_t distancer = 0;             // Variable to hold right ranger distance value
 
-float ksteer = 6;                   // Proportional Gain constant for steering
-float kdrive = 0.3;                   // Proportional Gain constant for Drive
+uint8_t front = 0xE0;               // Register location of the front ranger data
+uint8_t left = 0xE4;                // Register location of the left ranger data
+uint8_t right = 0xE6;               // Register location of the right ranger data
+
+uint8_t state = 1;                  // Operative variable for the main case statement, initial state is 1
+
+uint16_t des_distance = 50;         // Constant governing how close the car stays to the beacon while circling
+signed int distance_error = 0;      // distance_error = des_distance - distancer [set during read_ranger]
+
+float korbit = 6;                   // Proportional Gain constant used while orbiting the beacon
+float kturn = 0.3;                  // Proportional Gain constant used while turning towards/away from the beacon
 
 
 //__sbit __at 0xB0 SS;        // Sbit on Port 3 pin 0
-#define SS P3_0           // Simulator Sbit on Port 3 pin 0
+#define SS P3_0               // Simulator Sbit on Port 3 pin 0
 //__sbit __at 0xB0 PB;
 #define PB P2_1
 
@@ -76,79 +81,70 @@ void main(void){
     XBR_Init();
     SMB_Init();
 
-    // Read ranger once to trigger ping.
-
-    // Initialize the drive motor to neutral for 1 s
-    // Set motor pulsewidth to neutral
-    read_compass();
-    ReadRanger(front);
+    // Read from all rangers once REGARDLESS OF new_range flag to trigger a start ping.
+    // Because a sonar ping takes time to bounce off an object and return to the sensor, a start ping is necessary before the while loop.
+    read_ranger(front); // Use read_ranger() calls rather than update_rangers() to trigger the start ping regardless of new_range flag.
     distancef = distance;
-    ReadRanger(left);
+    read_ranger(left);
     distancel = distance;
-    ReadRanger(right);
+    read_ranger(right);
     distancer = distance;
-    MOTOR_PW = 0xFFFF-MOTOR_NEUT;
+
+    // Initialize the drive motor to neutral for 1 s. This is a requirement of the simulator.
+    MOTOR_PW = 0xFFFF-MOTOR_NEUT; // Set motor pulsewidth to neutral
     PCA0CP2 = MOTOR_PW;
     SERVO_PW = 0xFFFF-SERVO_CENTER;
     PCA0CP0 = SERVO_PW;
-    printf("Initializing motors, please wait.\n");
-    while(counts < 200){ // Wait some seconds
-        Sim_Update();   // Called in all loops!
+    printf("Initializing motors, please wait.\n"); // Print status of simulation
+    while(counts < 200){ // Until one second has elapsed...
+        Sim_Update();    // Refresh the simulation (necessary in all loops) and do not do anything else.
     }
 
     // Clear the flag counter variables to prevent a double read
     r_count = h_count = p_count = 0;
     new_range = new_heading = new_print = 0;
 
-    /*
-    states:
-    state 0:
-    state 1:
-    state 2:
-    state 3: activated upon pushing button
-    */
-
     // Make sure pushbutton is not pressed before starting
     while (!PB) {Sim_Update();}
     printf("\nPush Button Released... ");
+
+    // Wait for slide switch to toggle ON to begin program.
     if (!SS) {printf("\nPlease toggle the slide switch on. ");}
     while (!SS) {Sim_Update();}
+
     printf("Beginning Simulation:\n");
-    // Print data headers
+
+    // Print data headers for console output.
     printf("Time\tHeading\tRangeF\tRangeL\tRangeR\tServoPW\tMotorPW\tState\n");
     // Run program loop
-    // while(1) loop may or may not be needed, depending on how it's implemented.
     while(1){
-        // This stuff below is close to what a team should have had at the end of LITEC Lab3-3 (with pieces missing)
-        update_readings();
+
+        // Update the ranger and heading measurements if their respective flags are up.
+        update_rangers();
         update_heading();
-        //prints everything about ranger
+
+        // Print measurement readouts if new_print flag is up.
         print_info();
 
+        // Switch case statement governs how the robot moves - movement goal is divided into states.
         switch(state){
-            case(0):
+            case(1): // Initial state. The robot moves northeast towards desired heading 220deg.
                 set_motorPW(MOTOR_MAX);
-                if (distancer < 100) {
-                    //set_motorPW(MOTOR_NEUT); //Brief stop to minimize turn error
-                    state ++;}
-                break;
-            case(1):
-                set_motorPW(MOTOR_MAX);
-                heading_error = 2200 - heading;
-                set_servoPW((heading_error*kdrive)); //not sure if this will work, goal is to turn left
-                if(distancer < 100) { //EXPERIMENTALLY DEFINED: When car is sufficiently close to beacon
+                des_heading = 2200;
+                heading_error = des_heading - heading;
+                set_servoPW(heading_error*kturn); // Turn using experimentally derived PD constant for turns.
+                if(distancer < 100) { // Advance state when car is within 100 ranger units of target (limit experimentally derived).
                     state++;
                 }
                 break;
-            case(2):
-                set_servoPW(0-(distance_error*ksteer)); //Dom's circling code left unchanged
-                //Need to keep track of number of circles somehow, not sure of best way
-                if(!PB) {
+            case(2): // Target orbiting state.
+                set_servoPW(0-(distance_error*korbit)); // korbit experimentally derived to produce even circle
+                if(!PB) { // Advance state when pushbutton is depressed.
                     state++;
                 }
                 break;
-            case(3): //Return home
-                set_servoPW(0-(distance_error*ksteer));
+            case(3): // Continue orbiting until heading is sufficiently close to zero. This prevents collision with target.
+                set_servoPW(0-(distance_error*korbit));
                 if(heading < 100 || heading > 3500) {
                     state++;
                 }
@@ -156,11 +152,12 @@ void main(void){
             case(4):
                 if (heading < 100) {
                     heading_error = 0 - heading;
-                } else {
+                }
+                else {
                     heading_error = 3600 - heading;
                 }
 
-                set_servoPW((heading_error*kdrive));
+                set_servoPW((heading_error*kturn));
                 break;
 
         }
@@ -178,16 +175,16 @@ void print_info(){
     }
 }
 
-void update_readings(){
+void update_rangers(){
     if (new_range){
         // Get distance and act upon it
-        ReadRanger(front);
+        read_ranger(front);
         distancef = distance;
-        ReadRanger(left);
+        read_ranger(left);
         distancel = distance;
-        ReadRanger(right);
+        read_ranger(right);
         distancer = distance;
-        distance_error = desired_distance - distancer;
+        distance_error = des_distance - distancer;
         new_range = 0;
     }
 }
@@ -240,17 +237,7 @@ void read_compass(void){
                             //heading has units of 1/10 of a degree
 }
 
-void read_ranger(void){
-    unsigned char addr = 0xE0;          // the address of the ranger is 0xE0
-    unsigned char Data[2];              // Data is an array with length of 2
-    i2c_read_data(addr, 2, Data, 2);   //read two bytes, starting at reg 2
-    range = (((unsigned int)Data[0] << 8) | Data[1]);       // combine the two values
-    // Tell the ranger to trigger a ping
-    Data[0] = 0x51;                     // want to write 0x51 to reg 0 (ping in cm)
-    i2c_write_data(0xE0, 0, Data, 1);    // write data byte to ranger
-}
-
-void ReadRanger(uint8_t address){
+void read_ranger(uint8_t address){
     i2c_read_data(address, 2, Data, 2);   // Read the 0-3600 heading bytes
     distance = (((unsigned int)Data[0] << 8) | Data[1]);
     Data[0] = 0x51;
